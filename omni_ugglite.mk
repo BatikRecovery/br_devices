640e2ac5b83dedcba0b54da282403d84260c75d1
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2017-11-25 00:03:45 +0530

    ugglite: Set model based on ro.product.cert

--- omni_ugglite.mk
+++ omni_ugglite.mk
@@ -23,3 +23,6 @@ $(call inherit-product, device/xiaomi/ugglite/full_ugglite.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_NAME := omni_ugglite
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
+    ro.product.model
