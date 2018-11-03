32b484005c62d6c1e996730d84860d5ee280211d
Orkun <orkuncoskun9@gmail.com>
2018-11-03 14:54:02 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -33,10 +33,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Dalvik VM specific for devices with 2048 MB of RAM
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
-# Time Zone data for recovery
-PRODUCT_COPY_FILES += \
-    bionic/libc/zoneinfo/tzdata:$(TWRP_OUT)/system/usr/share/zoneinfo/tzdata
-
 # Display properties
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=480
@@ -44,9 +40,3 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 # USB properties
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.usb.pid_suffix=1D9
-
-# Default.prop overrides to get adb working at boot
-ADDITIONAL_DEFAULT_PROPERTIES += \
-    ro.adb.secure=0 \
-    ro.secure=0 \
-    ro.debuggable=1
