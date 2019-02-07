1c66de97171b14890b64fa2671e358cc29631284
TRISF <trisfaizal@gmail.com>
2019-02-07 10:23:46 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,6 +60,7 @@ BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 # Recovery
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
+AB_OTA_UPDATER := false
 
 # TWRP Configuration
 TW_THEME := portrait_hdpi
