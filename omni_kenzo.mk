2060c656eccd6f0a7042a9fe65fe0aa67baa98fc
ZHANtech™ <hifzhan41@gmail.com>
2019-06-04 07:19:43 +0700

    Batik Build

--- omni_kenzo.mk
+++ omni_kenzo.mk
@@ -4,7 +4,7 @@ PRODUCT_RELEASE_NAME := kenzo
 $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/batik/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kenzo
